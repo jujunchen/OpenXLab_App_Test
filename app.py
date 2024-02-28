4254b9533588784c42fa12dea66d9da0cbb1f150
阿提说说 <0431cjj@163.com>
2024-02-29 00:20:38 +0800

    fix

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ class InternLM_LLM(LLM):
         # model_path: InternLM 模型路径
         # 从本地初始化模型
         super().__init__()
-        print("正在从本地加载模型...")
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
-        self.model = self.model.eval()
-        print("完成本地模型的加载")
+        # print("正在从本地加载模型...")
+        # self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+        # self.model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+        # self.model = self.model.eval()
+        # print("完成本地模型的加载")
 
     def _call(self, prompt : str, stop: Optional[List[str]] = None,
                 run_manager: Optional[CallbackManagerForLLMRun] = None,
